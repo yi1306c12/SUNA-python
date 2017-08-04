d346dc3466167fcaed3254a1b9bf9918bf3c64b0
yi0x262 <yutadefn@gmail.com>
2017-08-04 09:22:40 +0900

    test case

--- novelty_map.py
+++ novelty_map.py
@@ -5,23 +5,24 @@ import networkx as nx
 class novelty_map(nx.Graph):
 
     def __init__(self,max_population):
+        assert max_population > 0, "max_population must be > 0"
         self.max_population = max_population
         super().__init__()
 
 
     def append(self,new):
         new_distances = [new.distance(n) for n in self.nodes()]
-        min_edge = self.min_weighted_edge()
 
         if len(self) < self.max_population:#if map is not full
             self.add_node(new)
             self.add_weighted_edges_from([(new,n,dist) for n,dist in zip(self.nodes(),new_distances)])#zip() iterates shortest times
             return True
 
-        elif min_edge[2]['weight'] < min(new_distances):#if map is full and new has more distance
+        min_edge = self.min_weighted_edge()
+        if min_edge[2]['weight'] < min(new_distances):#if map is full and new has more distance
             self.remove_node(min_edge[0])
-            distance_list = [new.distance(n) for n in self.nodes()]
             nodes_list = self.nodes()
+            distance_list = [new.distance(n) for n in nodes_list]
             self.add_node(new)
             self.add_weighted_edges_from([(new,n,dist) for n,dist in zip(nodes_list,distance_list)])
             return True
@@ -33,12 +34,26 @@ class novelty_map(nx.Graph):
 
 
 if __name__ == '__main__':
+    import random
+
     class test_gene(int):
         def distance(self,other):
-            return (self**2-other**2)**1
+            return (self-other)**2
+
     nm = novelty_map(100)
-    for i in range(150):
+    l = list(range(1000))
+    random.shuffle(l)
+    for i in l:
         nm.append(test_gene(i))
+    print(nm.nodes())
+    s = sorted(nm.nodes())
+    print(s)
+    sub = [a-b for a,b in zip(s[1:],s[:-1])]
+    print(sub)
+    print([sub.count(i) for i in range(20)])
+    print(list(range(20)))
+    print(sum([sub.count(i) for i in range(20)]))
+
 
     #pos = nx.shell_layout(nm)
     #nx.draw_networkx(nm,pos)
