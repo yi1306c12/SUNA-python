f2a4e2d73d1219dd8e94650287566b6883942e53
yi0x262 <yutadefn@gmail.com>
2017-08-28 22:12:10 +0900

    delete_connection

--- chromosome.py
+++ chromosome.py
@@ -4,6 +4,7 @@ from gene import neuron_gene, connection_gene
 normal_neurons = [identity_neuron,sigmoid_neuron,random_neuron,threshold_neuron]
 
 import random
+from functools import reduce
 
 class chromosome:
     def __init__(self, n_inputs, n_outputs, M_pa):
@@ -31,4 +32,10 @@ class chromosome:
         pass
 
     def delete_connection(self):
-        connection = random.choice(self.connections+self.control_neurons)
+        connections_lists = [self.connections, self.control_connections]
+        connection = random.choice(reduce(lambda x,y:x+y, connections_lists))
+
+        for c_list in connections_lists:
+            if connection in c_list:
+                c_list.remove(connection)
+                break
